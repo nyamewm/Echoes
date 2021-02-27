24f66ea0394cd3c04af9067cb160a2fbf4fbebd5
alex <lomonosov.aa@phystech.edu>
2021-02-27 19:01:50 +0300

    чистка кода

--- main.cpp
+++ main.cpp
@@ -7,35 +7,5 @@
 int main () {
     sf::RenderWindow window1(sf::VideoMode(740, 500), "Menu");
     menu(window1);
-    /*sf::RenderWindow window(sf::VideoMode(1000,1000), "Echoes");
-    Player a(&window);
-    while(window.isOpen())
-    {
-        sf::Event event;
-        while(window.pollEvent(event))
-        {
-            if(event.type == sf::Event::Closed)
-                window.close();
-        }
-        window.clear();
-
-        sf::Clock clock;
-        float time;
-        window.setVerticalSyncEnabled(true);
-
-
-        a.draw();
-
-        clock.restart();
-        time = clock.getElapsedTime().asMicroseconds();
-        a.update(time);
-        a.draw();
-
-
-
-
-
-        window.display();
-    }*/
     return 0;
 }
