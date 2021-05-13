a0e2c1ee65227c155d60e48ef30ef80574e9c7ea
andro <androsov.av@internet.ru>
2021-05-13 13:32:07 +0300

    sounds+animation+death

---
+++ savings.h
@@ -0,0 +1,8 @@
+#include <sstream>
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+#include "rapidjson/istreamwrapper.h"
+#include <fstream>
+
+using namespace rapidjson;
